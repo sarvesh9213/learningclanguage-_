a1528bc60510d5401e2723bf14efc7451fca1064
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2024-08-31 01:23:42 +0530

    Add files via upload

---
+++ multiplicationdiv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 8;// or u can just add int x,y ;
+              // then x = 8 or y = 3 ;  
+    int y = 3;
+    printf(" value of your x - y is %d \n", x - y);
+    printf(" dive x by y and u get %d", x / y);
+
+    return 0;
+}
